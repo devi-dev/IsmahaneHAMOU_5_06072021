f69cfaa5d2905914fca5c5079280db7d4f329099
isma <isma_hamou@hotmail.com>
2021-07-13 17:18:08 +0200

    création boucle

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,18 +1,29 @@
 fetch("http://localhost:3000/api/teddies")
-  .then(function(response) {
+  .then(function(response){
     if (response.ok){
-    return response.json()}
-    else{
-      console.error("retour du serveur: ", response.status)
+    return response.json()
     }
   })
-  .then(function (listOfProducts) {
-    console.log(listOfProducts)
-    class teddy{
-      constructor ()
+  .then(function (listOfProducts) {     
+    class article {
+      constructor(id,colors, name, imageUrl, description, price){
+        this.id = id;
+        this.colors = colors;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+      }
     }
-    for(let products of listOfProducts){
-      let productTeddy = new teddy(products);
-    }
-  })
+    
+    for (let articles of listOfProducts) {
+      let product = new article (articles)
+      document.querySelector(".card").innerHTML += ` <div class="card">
+                                                        <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
+                                                        <div class="card-body">
+                                                          <h2 class="card-title">${article.name}</h2>
+                                                          <div class="card-text description">${article.description}</div>
+                                                          <div class="card-text price">${article.price}</div>
+                                                        </div>`;
+  }})
   .catch(console.error);
