ce5b40216f86bc47fdfb95e0067d35841574a05e
isma <isma_hamou@hotmail.com>
2021-07-19 10:28:24 +0200

    script page

--- front-end/js/article.js
+++ front-end/js/article.js
@@ -1,1 +1,35 @@
-getelementbyid
+fetch("http://localhost:3000/api/teddies/${article._id}")
+  .then(function(response){
+    if (response.ok){
+    return response.json()
+    }
+  })
+  .then(function (listOfProducts) {  
+    console.log (listOfProducts);  
+    class articles {
+      constructor(_id, colors, name, imageUrl, description, price){
+        this._id = _id;
+        this.colors = colors;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+      }
+    }
+
+    for (let article of listOfProducts) {
+      document.querySelector(".container").innerHTML +=`  <div class="card bgprimary">
+                                                            <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?id=${article._id}>
+                                                              <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
+                                                              <div class="card-header bgsecondary">
+                                                                <h2 class="card-title">${article.name}</h2>
+                                                                <div class="card-text price">${article.price/ 100}.00 €</div>
+                                                              </div>
+                                                              <div class="card-body">
+                                                                <div class="card-text description">${article.description}</div>
+                                                              </div>
+                                                            </a>
+                                                          </div>`   
+
+    }})
+  .catch(console.error);
