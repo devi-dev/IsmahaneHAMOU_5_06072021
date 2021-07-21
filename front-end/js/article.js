803dcbcd8c9cc089473957551d215c926203e110
isma <isma_hamou@hotmail.com>
2021-07-21 19:13:25 +0200

    essaie pour récupérer les paramètre

--- front-end/js/article.js
+++ front-end/js/article.js
@@ -1,22 +1,6 @@
 
-let findUrl= window.location.search;
-let defineRegex = 'https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?';
-let getParameter = findUrl-defineRegex;
- if(getParameter.match(article._id)){
 
-    document.querySelector(".container").innerHTML(`<div class="card bgprimary">
-                                                        <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?_id:${article._id}>
-                                                          <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
-                                                            <div class="card-header bgsecondary">
-                                                              <h2 class="card-title">${article.name}</h2>
-                                                              <div class="card-text price">${article.price/ 100}.00 €</div>
-                                                              </div> 
-                                                              <div class="card-body">
-                                                                <div class="card-text description">${article.description}</div>
-                                                              </div>
-                                                        </a>
-                                                      </div>`        )}
- else{
-    
- }
+        let getParameters=window.location.search;
+        console.log(getParameters);
+
 
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -18,18 +18,22 @@ fetch("http://localhost:3000/api/teddies")
     }
 
     for (let article of listOfProducts) {
-      document.querySelector(".listofarticles").innerHTML += `<div class="card bgprimary">
-                                                                <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?_id:${article._id}>
-                                                                  <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
-                                                                  <div class="card-header bgsecondary">
-                                                                    <h2 class="card-title">${article.name}</h2>
-                                                                    <div class="card-text price">${article.price/ 100}.00 €</div>
-                                                                  </div>
-                                                                  <div class="card-body">
-                                                                    <div class="card-text description">${article.description}</div>
-                                                                  </div>
-                                                                </a>
-                                                              </div>`   
+      document.querySelector(".listofarticles").innerHTML +=`<div class="card bgprimary">
+                                                              <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?_id=${article._id} onclick="getParameters(this); return false">
+                                                                <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
+                                                                  <div>
+                                                                    <i class="far fa-heart fa-2x"></i>
+                                                                    <i class="fas fa-heart fa-2x"></i>
+                                                                   </div>
+                                                                <div class="card-header bgsecondary">
+                                                                  <h2 class="card-title">${article.name}</h2>
+                                                                  <div class="card-text price">${article.price/ 100}.00 €</div>
+                                                                </div>
+                                                                <div class="card-body">
+                                                                  <div class="card-text description">${article.description}</div>
+                                                                </div>
+                                                              </a>
+                                                            </div>`   
 
     }})
   .catch(console.error);
