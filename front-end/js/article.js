24cda081fd5b4157dd074aea9388800affac5c2f
isma <isma_hamou@hotmail.com>
2021-07-24 20:42:45 +0200

    test

--- front-end/js/article.js
+++ front-end/js/article.js
@@ -1,7 +1,7 @@
 //find the article's id from the URL
 const getParameters = window.location.search.replace("?","");
 
-//request the article information with url parameter =_id
+//request the article information with url parameter 
 fetch(`http://localhost:3000/api/teddies/${getParameters}`)
 
   .then(function(response){
@@ -13,7 +13,6 @@ fetch(`http://localhost:3000/api/teddies/${getParameters}`)
   .then(function (article) {  
        console.log (article);
        
-
         document.querySelector(".container").innerHTML +=`<div class="card bgprimary article-card">
                                                             <img class="card-img-top article-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
                                                             <div class="card-header bgsecondary article-card-header ">
@@ -24,8 +23,11 @@ fetch(`http://localhost:3000/api/teddies/${getParameters}`)
                                                                 <label for="color-select">Choisissez la couleur de votre ours en peluche:</label>
                                                                 <select name="colors" id="colors-select">
                                                                     <option value="">--Veuillez sélectionner une couleur--</option>
-                                                                    <option value="colors">${article.colors}</option>  
-                                                                </select></br>
+                                                                    <option value="colors">${article.colors[0]}</option>
+                                                                    <option value="colors">${article.colors[1]}</option>
+                                                                    <option value="colors">${article.colors[2]}</option>
+                                                                    <option value="colors">${article.colors[3]}</option>
+                                                                </select> </br>
                                                                 <select name="quantité" id="quantity-select">
                                                                     <option value="">--Quantité--</option>
                                                                     <option value="qty">1</option>
@@ -33,23 +35,19 @@ fetch(`http://localhost:3000/api/teddies/${getParameters}`)
                                                                     <option value="qty">3</option>
                                                                     <option value="qty">4</option>
                                                                     <option value="qty">5</option>
-                                                                    <option value="qty">6</option>
-                                                                    <option value="qty">7</option>
-                                                                    <option value="qty">8</option>
-                                                                    <option value="qty">9</option>
-                                                                    <option value="qty">10</option>
                                                                 </select>
                                                                 </br></br>
                                                                 <div class="card-text description" type="submit">${article.description}</div>
                                                                 <button class="add to cart">Ajouter au Panier</button>
                                                                 
                                                             </div>
-                                                         </div>`   
-    })
-
+                                                         </div>`
+   
+  })
   .catch(console.error);
-  /* 
-let colors =${article.colors};
-for (let i = 0; i < nombres.length; i++) {
-
-}
+  /*
+  const colors = article.colors
+      for (let color of colors { 
+      let newNode = document.createElement ("div"). innerhtml+= `<label for="color-select">Choisissez la couleur de votre choix:</label>
+                              <select name="color" class="color-select">${color[i]}</Select><br/>`
+      }*/
