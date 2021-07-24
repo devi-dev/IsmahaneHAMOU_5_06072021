cdcd6d2a259dc35477c28b6892febd28c03551a0
isma <isma_hamou@hotmail.com>
2021-07-24 15:49:44 +0200

    ajout élément page article

--- front-end/js/article.js
+++ front-end/js/article.js
@@ -13,28 +13,41 @@ fetch(`http://localhost:3000/api/teddies/${getParameters}`)
   .then(function (article) {  
        console.log (article);
 
-        document.querySelector(".container").innerHTML +=`<div class="card bgprimary">
-                                                              <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?${article._id}>
-                                                                <img class="card-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
-                                                                  <div>
-                                                                    <i class="far fa-heart fa-2x"></i>
-                                                                    <i class="fas fa-heart fa-2x"></i>
-                                                                   </div>
-                                                                <div class="card-header bgsecondary">
-                                                                  <h2 class="card-title">${article.name}</h2>
-                                                                  <div class="card-text price">${article.price/ 100}.00 €</div>
-                                                                </div>
-                                                                <div class="card-body">
-                                                                  <div class="card-text description">${article.description}</div>
-                                                                </div>
-                                                              </a>
-                                                            </div>`   
+        document.querySelector(".container").innerHTML +=`<div class="card bgprimary article-card">
+                                                            <img class="card-img-top article-img-top" src="${article.imageUrl}" alt="teddies" title=""/>
+                                                            <div class="card-header bgsecondary article-card-header ">
+                                                                <h2 class="card-title">${article.name}</h2>
+                                                                <div class="card-text price">${article.price/ 100}.00 €</div>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                <label for="color-select">Choisissez la couleur de votre ours en peluche:</label>
+                                                                <select name="colors" id="colors-select">
+                                                                    <option value="">--Veuillez sélectionner une couleur--</option>
+                                                                    <option value="colors">${article.colors[0]}</option>
+                                                                    <option value="colors">${article.colors[1]}</option>
+                                                                    <option value="colors">${article.colors[2]}</option>
+                                                                    <option value="colors">${article.colors[3]}</option>
+  
+                                                                </select></br>
+                                                                <select name="quantité" id="quantity-select">
+                                                                    <option value="">--Quantité--</option>
+                                                                    <option value="qty">1</option>
+                                                                    <option value="qty">2</option>
+                                                                    <option value="qty">3</option>
+                                                                    <option value="qty">4</option>
+                                                                    <option value="qty">5</option>
+                                                                    <option value="qty">6</option>
+                                                                    <option value="qty">7</option>
+                                                                    <option value="qty">8</option>
+                                                                    <option value="qty">9</option>
+                                                                    <option value="qty">10</option>
+                                                                </select>
+                                                                </br></br>
+                                                                <div class="card-text description" type="submit">${article.description}</div>
+                                                                <button class="add to cart">Ajouter au Panier</button>
+                                                                
+                                                            </div>
+                                                         </div>`   
     })
 
   .catch(console.error);
-
-
-
-// comparer l'id de l'url avec celui requeter par l'api en utilisant switch
-
-//console.log (idApi)
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -9,6 +9,8 @@ fetch("http://localhost:3000/api/teddies")
 
   .then(function (listOfProducts) {  
     console.log (listOfProducts); 
+
+    //Hydrate page with the list of articles
     for (let article of listOfProducts) {
       document.querySelector(".listofarticles").innerHTML +=`<div class="card bgprimary">
                                                               <a href=https://devi-dev.github.io/IsmahaneHAMOU_5_06072021/article.html?${article._id}>
